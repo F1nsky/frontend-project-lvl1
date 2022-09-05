c3cbc79a9028c9243eccfd4b6468fc5b3d4e168b
Finsky <VasiliyWinter@gmail.com>
2022-09-05 15:10:41 +0700

    update files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import { userName } from '/home/finsky/frontend-project-lvl1/src/cli.js';
+
+console.log('Hello, ' + userName + '!');
