53a750b7b14125a4c70d769423f455fbd1e875ec
Finsky <VasiliyWinter@gmail.com>
2022-09-17 16:23:33 +0700

    update game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,10 +1,17 @@
-import engine from '../index.js';
-import getRandomNum from '../getRandomNum.js';
-import getRandomOperator from '../getRandomOperator.js';
+// импортируем логику из файла 'index.js'
+import gameEngine from '../index.js';
 
-export default (userName) => {
-  const firstOperation = `${getRandomNum()} ${getRandomOperator()} ${getRandomNum()}`;
-  const secondOperation = `${getRandomNum()} ${getRandomOperator()} ${getRandomNum()}`;
-  const thirdOperation = `${getRandomNum()} ${getRandomOperator()} ${getRandomNum()}`;
-  engine(userName, firstOperation, secondOperation, thirdOperation);
+// экспортируем текст правил игры
+export const rulesDescription = 'What is the result of the expression?';
+
+// объявляем константу производящую вычисление правильного ответа
+const getCorrectAnswer = (randomNum1, randomNum2, randomOperator) => {
+  // вычисляем результат случайного выражения с помощью метода "eval"
+  const evaluation = eval(`${randomNum1} ${randomOperator} ${randomNum2}`);
+  // округляем результат до десятых
+  return Math.round(((evaluation) + Number.EPSILON) * 10) / 10;
 };
+
+// экспортируем игру gameCalc
+// с логикой gameEngine для которой указаны параметры (rulesDescription, getCorrectAnswer)
+export const gameCalc = () => gameEngine(rulesDescription, getCorrectAnswer);
