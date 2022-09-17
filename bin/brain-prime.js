061c82dfa7e6556e5aec25af06cdc23da87dd132
Finsky <VasiliyWinter@gmail.com>
2022-09-17 16:20:03 +0700

    update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,63 +1,9 @@
 #!/usr/bin/env node
+// шебанг с интерпретатором node
+// необходим для запуска файла brain-prime.js как программы по команде brain-prime
 
-import getRandomNum from '../src/getRandomNum.js';
-import getAnswer from '../src/cli.js';
+// импортируем функцию gameCalc из файла 'games/game-prime.js'
+import { gamePrime } from '../src/games/game-prime.js';
 
-let winCount = 0;
-let loseCount = 0;
-let result = '';
-
-console.log('Welcome to the Brain Games!');
-
-const userName = getAnswer('May I have your name? ');
-
-console.log(`Hello, ${userName}!`); // задаем вопрос 'May I have your name?' и  подставляем ответ пользователя в текст 'Hello, {имя пользователя}!'
-
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-while (winCount < 3 && loseCount < 1) {
-  const randomNum = getRandomNum();
-  let conclusion = true;
-
-  const isPrime = () => {
-    if (randomNum <= 1) {
-      conclusion = false;
-    }
-
-    for (let i = 2; i < randomNum; i += 1) {
-      if (randomNum % i === 0) {
-        conclusion = false;
-      }
-    }
-    return conclusion;
-  };
-
-  const correctAnswer = isPrime();
-
-  const userAnswer = getAnswer(`Question: ${randomNum} `);
-  // задаем вопрос 'Question:' и подставлем ответ в текст 'Your answer: {ответ пользователя}'
-  console.log(`Your answer: ${userAnswer}`);
-  if (userAnswer === correctAnswer.toString()) {
-    console.log('Correct!');
-    winCount += 1;
-  } else {
-    console.log(` '${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    loseCount += 1;
-  }
-}
-
-const lostGame = `Let's try again, ${userName}!`;
-const winGame = `Congratulations, ${userName}!`;
-
-const checkResult = () => {
-  if (winCount === 3) {
-    result = winGame;
-  }
-
-  if (loseCount > 0) {
-    result = lostGame;
-  }
-  return result;
-};
-
-console.log(checkResult());
+// запускаем функцию
+gamePrime();
