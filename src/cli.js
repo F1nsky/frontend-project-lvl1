97ca9009fca4de2f72d0cebce8c5be35cd286bce
Finsky <VasiliyWinter@gmail.com>
2022-09-17 16:21:23 +0700

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,5 @@
+// импортируем библиотеку 'readline-sync': https://github.com/anseki/readline-sync
 import readlineSync from 'readline-sync';
 
+// экспортируем функцию позволяющую спросить у пользователя вопрос, записать и отобразить ответ
 export default (question) => readlineSync.question(`${question}`);
