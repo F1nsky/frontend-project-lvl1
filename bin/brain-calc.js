a1478c3fc014c5df685860f4f7c1f294a5cf29ee
Finsky <VasiliyWinter@gmail.com>
2022-10-05 23:33:16 +0700

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// шебанг с интерпретатором node
-// необходим для запуска файла brain-calc.js как программы по команде brain-calc
-
-// импортируем функцию gameCalc из файла 'games/game-calc.js'
 import { gameCalc } from '../src/games/game-calc.js';
 
-// запускаем функцию
 gameCalc();
