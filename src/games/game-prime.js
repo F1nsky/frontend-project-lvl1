5e30b293a734dfab735098be715f0c5f6e84c801
Finsky <VasiliyWinter@gmail.com>
2022-10-05 23:36:40 +0700

    update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,35 +1,28 @@
-// импортируем логику из файла 'index.js'
-import gameEngine from '../index.js';
+import gameEngine from '../index-test.js';
+import getAnswer from '../cli.js';
+import getRandomNum from '../getRandomNum.js';
 
-// экспортируем текст правил игры
-export const rulesDescription = "'Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
-// объявляем переменную в которую сохраняется правильный ответ
-// по умолчанию в ней указан ответ 'yes'
-let conclusion = 'yes';
+let randomNum;
 
-// объявляем константу производящую вычисление правильного ответа
-const getCorrectAnswer = (randomNum) => {
-  // если случайное число меньше или равно 1,
-  // значит оно не простое и в conclusion сохраняется 'no'
-  if (randomNum <= 1) {
-    conclusion = 'no';
-  }
-
-  // объявляем цикл, который проверяет случайное число на соотвествие условию
-  // и возвращает переменную conclusion со значением 'no' или 'yes' в зависимости от результата
-  for (let i = 2; i < randomNum; i += 1) {
-    // если остаток деления randomNum на i равен нулю
-    // то в переменную conclusion сохраняется 'no'
-    if (randomNum % i === 0) {
-      conclusion = 'no';
-    }
-    // в других случаях сохранения значений в переменную conclusion не происходит
-  }
-  // производится возврат переменной conclusion
-  return conclusion;
+const question = () => {
+  randomNum = getRandomNum();
+  const result = getAnswer(`Question: ${randomNum} `);
+  return result;
 };
 
-// экспортируем игру gamePrime
-// с логикой gameEngine для которой указаны параметры (rulesDescription, getCorrectAnswer)
-export const gamePrime = () => gameEngine(rulesDescription, getCorrectAnswer);
+const getCorrectAnswer = () => {
+  let result = 'yes';
+  if (randomNum <= 1) {
+    result = 'no';
+  }
+  for (let i = 2; i < randomNum; i += 1) {
+    if (randomNum % i === 0) {
+      result = 'no';
+    }
+  }
+  return result;
+};
+
+export const gamePrime = () => gameEngine(question, getCorrectAnswer);
