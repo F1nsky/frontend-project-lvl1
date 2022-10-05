dfbe444597e41e71c1d970e1c11f3c9404da5eaf
Finsky <VasiliyWinter@gmail.com>
2022-10-05 23:34:24 +0700

    update brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// шебанг с интерпретатором node
-// необходим для запуска файла brain-progression.js как программы по команде brain-progression
-
-// импортируем функцию gameCalc из файла 'games/game-progression.js'
 import { gameProgression } from '../src/games/game-progression.js';
 
-// запускаем функцию
 gameProgression();
