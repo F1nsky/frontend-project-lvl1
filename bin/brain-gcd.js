e2f018afbde259f442b35511f51f9f07235984de
Finsky <VasiliyWinter@gmail.com>
2022-10-05 23:35:11 +0700

    update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// шебанг с интерпретатором node
-// необходим для запуска файла brain-gcd.js как программы по команде brain-gcd
-
-// импортируем функцию gameCalc из файла 'games/game-gcd.js'
 import { gameGrComDiv } from '../src/games/game-gcd.js';
 
-// запускаем функцию
 gameGrComDiv();
