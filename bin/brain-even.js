1f057e30e86736e002e25687d8250fe67d1a6f08
Finsky <VasiliyWinter@gmail.com>
2022-10-05 23:33:33 +0700

    update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// шебанг с интерпретатором node
-// необходим для запуска файла brain-even.js как программы по команде brain-even
-
-// импортируем функцию gameCalc из файла 'games/game-even.js'
 import { gameEven } from '../src/games/game-even.js';
 
-// запускаем функцию
 gameEven();
