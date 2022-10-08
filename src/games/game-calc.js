d608a29036fb1b4ee745f479a5af7e6811fbb1a8
Finsky <VasiliyWinter@gmail.com>
2022-10-08 12:54:59 +0700

    update game-calc.js

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -2,38 +2,41 @@ import gameEngine from '../index.js';
 import getAnswer from '../cli.js';
 import getRandomNum from '../getRandomNum.js';
 
-const getRandomOperator = () => {
-  const operators = {
-    0: '+',
-    1: '-',
-    2: '*',
+const generateRound = () => {
+  const getRandomOperator = () => {
+    const operators = {
+      0: '+',
+      1: '-',
+      2: '*',
+    };
+    const num = getRandomNum(0, 2);
+
+    return operators[num];
   };
-  const num = getRandomNum(0, 2);
 
-  return operators[num];
-};
+  console.log('What is the result of the expression?');
 
-console.log('What is the result of the expression?');
+  let randomNum1;
+  let randomNum2;
+  let randomOperator;
 
-let randomNum1;
-let randomNum2;
-let randomOperator;
-
-const question = () => {
-  randomNum1 = getRandomNum();
-  randomNum2 = getRandomNum();
-  randomOperator = getRandomOperator();
-  const result = getAnswer(`Question: ${randomNum1} ${randomOperator} ${randomNum2} `);
-  return result;
-};
-
-const getCorrectAnswer = () => {
-  const expressions = {
-    '+': randomNum1 + randomNum2,
-    '-': randomNum1 - randomNum2,
-    '*': randomNum1 * randomNum2,
+  const question = () => {
+    randomNum1 = getRandomNum();
+    randomNum2 = getRandomNum();
+    randomOperator = getRandomOperator();
+    const result = getAnswer(`Question: ${randomNum1} ${randomOperator} ${randomNum2} `);
+    return result;
   };
-  return expressions[randomOperator];
+
+  const getCorrectAnswer = () => {
+    const expressions = {
+      '+': randomNum1 + randomNum2,
+      '-': randomNum1 - randomNum2,
+      '*': randomNum1 * randomNum2,
+    };
+    return expressions[randomOperator];
+  };
+  return [question(), getCorrectAnswer()];
 };
 
-export default () => gameEngine(question, getCorrectAnswer);
+export default () => gameEngine(generateRound);
