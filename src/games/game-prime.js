66f2eed31834998796c94c46a343548cce7eee1c
Finsky <VasiliyWinter@gmail.com>
2022-10-08 12:55:30 +0700

    update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,24 +4,27 @@ import getRandomNum from '../getRandomNum.js';
 
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-let randomNum;
+const generateRound = () => {
+  let randomNum;
 
-const question = () => {
-  randomNum = getRandomNum();
-  return getAnswer(`Question: ${randomNum} `);
-};
+  const question = () => {
+    randomNum = getRandomNum();
+    return getAnswer(`Question: ${randomNum} `);
+  };
 
-const getCorrectAnswer = () => {
-  let result = 'yes';
-  if (randomNum <= 1) {
-    result = 'no';
-  }
-  for (let i = 2; i < randomNum; i += 1) {
-    if (randomNum % i === 0) {
+  const getCorrectAnswer = () => {
+    let result = 'yes';
+    if (randomNum <= 1) {
       result = 'no';
     }
-  }
-  return result;
+    for (let i = 2; i < Math.sqrt(randomNum); i += 1) {
+      if (randomNum % i === 0) {
+        result = 'no';
+      }
+    }
+    return result;
+  };
+  return [question(), getCorrectAnswer()];
 };
 
-export default () => gameEngine(question, getCorrectAnswer);
+export default () => gameEngine(generateRound);
