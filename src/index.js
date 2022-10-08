b41f6ad394444853f03f0c2cc77dc6f2c3628d6a
Finsky <VasiliyWinter@gmail.com>
2022-10-08 12:56:08 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ console.log('Welcome to the Brain Games!');
 const userName = getAnswer('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 
-export default (question, getCorrectAnswer) => {
+export default (generateRound) => {
   let result;
   for (let winCount = 0; winCount < 3;) {
-    const userAnswer = question();
+    const [userAnswer, correctAnswer] = generateRound();
     console.log(`Your answer: ${userAnswer}`);
-    const correctAnswer = getCorrectAnswer();
     if (userAnswer !== correctAnswer.toString()) {
       result = console.log(` '${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       return result;
