457d3ff999231fc13da5af4de7193a43f0e8bcc6
Finsky <VasiliyWinter@gmail.com>
2022-10-08 13:25:39 +0700

    update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -6,13 +6,14 @@ console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const generateRound = () => {
   const randomNum = getRandomNum();
+
   const question = () => getAnswer(`Question: ${randomNum} `);
+
   const getCorrectAnswer = () => {
     let result = 'yes';
     for (let i = 2; i < Math.sqrt(randomNum); i += 1) {
-      if (randomNum % i === 0) {
+      if (randomNum % i === 0 || randomNum <= 1) {
         result = 'no';
-        return result;
       }
     }
     return result;
