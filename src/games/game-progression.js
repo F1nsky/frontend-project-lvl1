08de4bd11f3f11bd48eafc98ea365f6c96e6c33d
Finsky <VasiliyWinter@gmail.com>
2022-10-08 12:55:48 +0700

    update game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -4,37 +4,40 @@ import getRandomNum from '../getRandomNum.js';
 
 console.log('What number is missing in the progression?');
 
-let firstItem;
-let increment;
-let progressionLength;
-let randomArray;
-let replaceIndex;
+const generateRound = () => {
+  let firstItem;
+  let increment;
+  let progressionLength;
+  let randomArray;
+  let replaceIndex;
 
-const getRandomArray = () => {
-  firstItem = getRandomNum();
-  increment = getRandomNum();
-  progressionLength = getRandomNum(5, 10);
-  const array = [firstItem];
-  for (let i = 1; array.length <= progressionLength; i += 1) {
-    array[i] = array[i - 1] + increment;
-  }
-  return array;
-};
-
-const question = () => {
-  randomArray = getRandomArray();
-  const progression = [...randomArray];
-  replaceIndex = getRandomNum(0, progressionLength);
-
-  const randomHiddenItem = () => {
-    progression.splice(replaceIndex, 1, '..');
-    return progression.toString();
+  const getRandomArray = () => {
+    firstItem = getRandomNum();
+    increment = getRandomNum();
+    progressionLength = getRandomNum(5, 10);
+    const array = [firstItem];
+    for (let i = 1; array.length <= progressionLength; i += 1) {
+      array[i] = array[i - 1] + increment;
+    }
+    return array;
   };
-  const addHiddenItem = randomHiddenItem();
-  const processedQuestion = addHiddenItem.replaceAll(',', ' ');
-  return getAnswer(`Question: ${processedQuestion} `);
+
+  const question = () => {
+    randomArray = getRandomArray();
+    const progression = [...randomArray];
+    replaceIndex = getRandomNum(0, progressionLength);
+
+    const randomHiddenItem = () => {
+      progression.splice(replaceIndex, 1, '..');
+      return progression.toString();
+    };
+    const addHiddenItem = randomHiddenItem();
+    const processedQuestion = addHiddenItem.replaceAll(',', ' ');
+    return getAnswer(`Question: ${processedQuestion} `);
+  };
+
+  const getCorrectAnswer = () => randomArray[replaceIndex];
+  return [question(), getCorrectAnswer()];
 };
 
-const getCorrectAnswer = () => randomArray[replaceIndex];
-
-export default () => gameEngine(question, getCorrectAnswer);
+export default () => gameEngine(generateRound);
