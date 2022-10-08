a8f73028ee10aafeefc0ca527c6a13e5af173c8c
Finsky <VasiliyWinter@gmail.com>
2022-10-08 12:55:19 +0700

    update game-gcd.js

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -4,23 +4,26 @@ import getRandomNum from '../getRandomNum.js';
 
 console.log('Find the greatest common divisor of given numbers.');
 
-let randomNum1;
-let randomNum2;
+const generateRound = () => {
+  let randomNum1;
+  let randomNum2;
 
-const question = () => {
-  randomNum1 = getRandomNum();
-  randomNum2 = getRandomNum();
-  const result = getAnswer(`Question: ${randomNum1} ${randomNum2} `);
-  return result;
+  const question = () => {
+    randomNum1 = getRandomNum();
+    randomNum2 = getRandomNum();
+    const result = getAnswer(`Question: ${randomNum1} ${randomNum2} `);
+    return result;
+  };
+
+  const getCorrectAnswer = () => {
+    while (randomNum2) {
+      const temp = randomNum2;
+      randomNum2 = randomNum1 % randomNum2;
+      randomNum1 = temp;
+    }
+    return randomNum1;
+  };
+  return [question(), getCorrectAnswer()];
 };
 
-const getCorrectAnswer = () => {
-  while (randomNum2) {
-    const temp = randomNum2;
-    randomNum2 = randomNum1 % randomNum2;
-    randomNum1 = temp;
-  }
-  return randomNum1;
-};
-
-export default () => gameEngine(question, getCorrectAnswer);
+export default () => gameEngine(generateRound);
