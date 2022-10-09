39f1007146f21dde35759e68efebf4af018bce98
Finsky <VasiliyWinter@gmail.com>
2022-10-09 20:35:29 +0700

    update game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -4,36 +4,32 @@ import getRandomNum from '../getRandomNum.js';
 
 console.log('What number is missing in the progression?');
 
+const getProgression = (start, step, end) => {
+  const array = [start];
+  for (let i = 1; array.length <= end; i += 1) {
+    array[i] = array[i - 1] + step;
+  }
+  return array;
+};
+
 const generateRound = () => {
-  const firstItem = getRandomNum();
-  const increment = getRandomNum();
-  const progressionLength = getRandomNum(5, 10);
+  const start = getRandomNum();
+  const step = getRandomNum();
+  const end = getRandomNum(5, 10);
 
-  const getRandomArray = () => {
-    const array = [firstItem];
-    for (let i = 1; array.length <= progressionLength; i += 1) {
-      array[i] = array[i - 1] + increment;
-    }
-    return array;
+  const randomArray = getProgression(start, step, end);
+  const replaceIndex = getRandomNum(0, end);
+  const progression = [...randomArray];
+  const randomHiddenItem = () => {
+    progression.splice(replaceIndex, 1, '..');
+    return progression.toString().replaceAll(',', ' ');
   };
+  const processedQuestion = randomHiddenItem();
 
-  const randomArray = getRandomArray();
-  const replaceIndex = getRandomNum(0, progressionLength);
+  const question = getAnswer(`Question: ${processedQuestion} `);
+  const correctAnswer = randomArray[replaceIndex];
 
-  const question = () => {
-    const progression = [...randomArray];
-
-    const randomHiddenItem = () => {
-      progression.splice(replaceIndex, 1, '..');
-      return progression.toString();
-    };
-    const addHiddenItem = randomHiddenItem();
-    const processedQuestion = addHiddenItem.replaceAll(',', ' ');
-    return getAnswer(`Question: ${processedQuestion} `);
-  };
-
-  const getCorrectAnswer = () => randomArray[replaceIndex];
-  return [question(), getCorrectAnswer()];
+  return [question, correctAnswer];
 };
 
 export default () => gameEngine(generateRound);
